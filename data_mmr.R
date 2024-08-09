816bcfe7aff6e52dc9acc0c3313948d4a09f7df0
Nina Mayer <nina.mayer4.4@freenet.de>
2024-08-09 14:09:35 +0200

    data

--- data_mmr.R
+++ data_mmr.R
@@ -1,1 +1,7 @@
-untar("./data.tar.xz", verbose = TRUE)
+library(haven)
+library(archive)
+
+HonestDid <- read.csv("HonestDiD.csv")
+IndiaMMR <- read.csv("IndiaStateMMRQuotas.csv")
+worldwide_quotas <- read_dta("worldwide_quotas.dta")
+
